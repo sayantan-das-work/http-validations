61f51c713688db1650a1cfbc0fa0b33d7e5327c1
Sayantan Das <sayantandas768.work@gmail.com>
2024-09-08 23:01:59 +0530

    Removed unnecessary imports

--- http/src/main/java/io/github/sayantandaswork/httpvalidation/exception/handlers/MethodArgNotValidExHandler.java
+++ http/src/main/java/io/github/sayantandaswork/httpvalidation/exception/handlers/MethodArgNotValidExHandler.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
